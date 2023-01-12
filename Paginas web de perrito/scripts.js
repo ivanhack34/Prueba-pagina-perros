5afa6283cfccff96e109e22c39b8ddf37273443d
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2023-01-12 16:14:11 -0400

    Pagina web perrito vbeta
    Esta es mi pagina web de perritos la version beta

---
+++ Paginas web de perrito/scripts.js
@@ -0,0 +1,10 @@
+//Función para mostrar una alerta cuando se haga click en el botón "Adoptar"
+function mostrarAlerta() {
+    alert("¡Gracias por considerar la adopción de un perro!");
+  }
+  
+  //Función para cambiar el texto de un elemento con el id "mensaje" al hacer hover sobre él
+  function cambiarTexto() {
+    document.getElementById("mensaje").innerHTML = "¡Manten a tu perro saludable y feliz!";
+  }
+  
